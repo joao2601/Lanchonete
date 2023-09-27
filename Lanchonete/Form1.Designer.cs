9f21667c166cc6d0e48e7ad0659cedbfd2673c23
joao2601 <jaomarcondes26@gmail.com>
2023-09-27 12:45:52 -0300

    .......

--- Lanchonete/Form1.Designer.cs
+++ Lanchonete/Form1.Designer.cs
@@ -40,11 +40,20 @@
             this.label5 = new System.Windows.Forms.Label();
             this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
             this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(610, 401);
+            this.button1.Location = new System.Drawing.Point(604, 233);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(166, 37);
             this.button1.TabIndex = 5;
@@ -153,6 +162,70 @@
             this.maskedTextBox2.TabIndex = 4;
             this.maskedTextBox2.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox2_MaskInputRejected);
             // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader6,
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(243, 25);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(336, 114);
+            this.listView1.TabIndex = 11;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Id";
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Nome";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Email";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Senha";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Telefone";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Cpf";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(602, 293);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 37);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "EDITAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(604, 348);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(166, 37);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "EXCLUIR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -161,6 +234,9 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.maskedTextBox2);
             this.Controls.Add(this.maskedTextBox1);
             this.Controls.Add(this.label4);
@@ -195,6 +271,15 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.MaskedTextBox maskedTextBox1;
         private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Lanchonete/Form1.cs
+++ Lanchonete/Form1.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Common;
 using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
@@ -12,8 +14,10 @@ using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Lanchonete
 {
+
     public partial class Form1 : Form
     {
+        private int Id;
         public Form1()
         {
             InitializeComponent();
@@ -21,8 +25,51 @@ namespace Lanchonete
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            UpdateListView();
         }
+
+        private void UpdateListView()
+        {
+            listView1.Items.Clear();
+
+           conexao conn = new conexao();
+            SqlCommand sqlCom = new SqlCommand();
+
+            sqlCom.Connection = conn.ReturnConnection();
+            sqlCom.CommandText = "SELECT * FROM Cadastro";
+
+            try
+            {
+                SqlDataReader dr = sqlCom.ExecuteReader();
+
+                //Enquanto for possível continuar a leitura das linhas que foram retornadas na consulta, execute.
+                while (dr.Read())
+                {
+                    int id = (int)dr["id"];
+                    string name = (string)dr["Nome"];
+                    string email = (string)dr["email"];
+                    string cpf = (string)dr["senha"];
+                    string telefone = (string)dr["telefone"];
+                   
+                    ListViewItem lv = new ListViewItem(id.ToString());
+                    lv.SubItems.Add(name);
+                    lv.SubItems.Add(email);
+                    lv.SubItems.Add(cpf);
+                    listView1.Items.Add(lv);
+
+                }
+                dr.Close();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+            finally
+            {
+                conn.CloseConnection();
+            }
+        }
+    
 
 
 
@@ -112,6 +159,85 @@ namespace Lanchonete
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            conexao connection = new conexao();
+            SqlCommand sqlCommand = new SqlCommand();
+
+            sqlCommand.Connection = connection.ReturnConnection();
+            sqlCommand.CommandText = @"UPDATE  Cadastro SET
+               Nome = @textBox1.Text,
+               Email = @email.Text,
+               Senha = @textBox3.Text,
+               Telefone = @maskedTextBox1.Text,
+               Cpf = @maskedTextBox2.Text
+               WHERE Id = @Id";
+
+            sqlCommand.Parameters.AddWithValue("@Nome", textBox1.Text);
+            sqlCommand.Parameters.AddWithValue("@Email", email.Text);
+            sqlCommand.Parameters.AddWithValue("@Senha", textBox3.Text);
+            sqlCommand.Parameters.AddWithValue("@Telefone", maskedTextBox1.Text);
+            sqlCommand.Parameters.AddWithValue("@Cpf", maskedTextBox2.Text);
+            sqlCommand.Parameters.AddWithValue("@Id", Id);
+
+
+            sqlCommand.ExecuteNonQuery();
+
+
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            int index;
+            index = listView1.FocusedItem.Index;
+            Id = int.Parse(listView1.Items[index].SubItems[0].Text);
+            textBox1.Text = (listView1.Items[index].SubItems[1].Text);
+            email.Text = listView1.Items[index].SubItems[2].Text;
+            textBox3.Text = listView1.Items[index].SubItems[3].Text;
+            maskedTextBox1.Text = listView1.Items[index].SubItems[4].Text;
+            maskedTextBox2.Text = listView1.Items[index].SubItems[5].Text;
+           
+
+
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Código para excluir
+            conexao connection = new conexao();
+            SqlCommand sqlCommand = new SqlCommand();
+
+            sqlCommand.Connection = connection.ReturnConnection();
+            sqlCommand.CommandText = @"DELETE FROM Usuarios WHERE Id = @id";
+            sqlCommand.Parameters.AddWithValue("@id", Id);
+            try
+            {
+                sqlCommand.ExecuteNonQuery();
+            }
+            catch (Exception err)
+            {
+                throw new Exception("Erro: Problemas ao excluir usuário no banco.\n" + err.Message);
+            }
+            finally
+            {
+                connection.CloseConnection();
+                textBox1.Clear();
+                email.Clear();
+                textBox3.Clear();
+                maskedTextBox1.Clear();
+                maskedTextBox2.Clear();
+
+                UpdateListView();
+            }
 
         }
     }
